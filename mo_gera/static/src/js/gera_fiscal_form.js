72c41b332bc4ec59cc84e1b6e44bd07df54d8e33
ibaev <WlcLrbnU59>
2020-04-15 16:05:42 +0300

    [UPDATE] Minor updates

--- mo_gera/static/src/js/gera_fiscal_form.js
+++ mo_gera/static/src/js/gera_fiscal_form.js
@@ -133,14 +133,16 @@ FormController.include({
                 '',
                 ''
             ).then(function (res) {
-               if ((!res.result && res.error.message) || (!res.result && !res.error)){
-                   var error =  res.error
-                   if (!res.result && res.error) {
-                        self.do_warn(_t("Error"), _t("An error occurred during printing receipt. " + error.data.message));
-                   } else {
-                        self.do_warn(_t("Error"), _t("An error occurred during printing receipt."));
-                   }
-               }
+               if (res.result != true && res.result != false && !res.error){
+                    var error =  res.error
+                    if (!res.result && res.error) {
+                         self.do_warn(_t("Error"), _t("An error occurred during printing receipt. ") + res.result);
+                    } else if (!res.result && !error){
+                         self.do_warn(_t("Error"), _t("An error occurred during printing receipt."));
+                    } else {
+                         self.do_warn(_t("Error"), _t("An error occurred during printing receipt. ") + error.data.message);
+                    }
+                }
             });
         }
      }
